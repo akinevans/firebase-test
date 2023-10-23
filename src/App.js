efa17f2ec3440f2f3506416e57a1bed3441640a1
Akin Evans <akinevans@gmail.com>
2023-10-23 16:18:47 -0700

    Update comments

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ import { getAnalytics } from "firebase/analytics";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+//^ Note, firebase API key is OK to share, its used only to recognize your app
 const firebaseConfig = {
   apiKey: "AIzaSyDgrOgukT6wjoioWX7wgVi9XzNlvp3FGqg",
   authDomain: "fir-login-test-aabe2.firebaseapp.com",
