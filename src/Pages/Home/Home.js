02a0fe8619e58ee37c7400cfc623f9041d399f52
Akin Evans <akinevans@gmail.com>
2023-10-23 18:09:50 -0700

    Add links to home page

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,9 +1,19 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Home() {
   return (
-    <div className='flex justify-center items-center h-[200px]'>
+    <div className='flex flex-col justify-center items-center h-[200px]'>
       <h1 className='text-[30px]'>Home Page...</h1>
+      <Link to='/login' className='text-blue-600'>
+        Login
+      </Link>
+      <Link to='/forgot-password' className='text-blue-600'>
+        Forgot Password
+      </Link>
+      <Link to='/reset-password' className='text-blue-600'>
+        Reset Password
+      </Link>
     </div>
   );
 }
